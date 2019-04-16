b7d7b1a3741c02fd8352d5e0bc66de1968848ef1
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2019-04-16 14:54:15 +0100

    ch(host-app): Host app on heroku
    - adds development and production environment for docker
    - hosts app on heroku on https://airtech-api.herokuapp.com
    
    [Maintains #165386785]

--- celery_config/celery_schedules.py
+++ celery_config/celery_schedules.py
@@ -1,12 +1,2 @@
 from . import celery_scheduler
 # from celery.schedules import crontab
-
-celery_scheduler.conf.beat_schedule = {
-    # This cronjob is used to test that celery_config is properly set up
-    # It executes every 60 seconds
-    'add-every-100-hours': {
-        'task': 'add_number',
-        'schedule': 6.0,
-        'args': (16, 16)
-    }
-}
