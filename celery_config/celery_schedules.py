b0e5a6d386839fcc4d2a7ed2932555b5f0e4dfcb
Chidiebere Oguejiofor <chidioguejiofor@gmail.com>
2019-07-15 16:39:07 +0100

    ch(update-heroku): Update heroku app
    - correct bug in heroku app

--- celery_config/celery_schedules.py
+++ celery_config/celery_schedules.py
@@ -1,11 +1,11 @@
-# from . import celery_scheduler
-# from celery.schedules import crontab
-#
-# celery_scheduler.conf.beat_schedule = {
-#     # This cronjob is used to test that celery_config is properly set up
-#     # It executes every 60 seconds
-#     'notify-user-of-bookings-about-to-expired-every-12-hours': {
-#         'task': 'add_number',
-#         'schedule':crontab(hour='0,12,18'),
-#     }
-# }
+from . import celery_scheduler
+from celery.schedules import crontab
+
+celery_scheduler.conf.beat_schedule = {
+    # This cronjob is used to test that celery_config is properly set up
+    # It executes every 60 seconds
+    # 'notify-user-of-bookings-about-to-expired-every-12-hours': {
+    #     'task': 'add_number',
+    #     'schedule':crontab(hour='0,12,18'),
+    # }
+}
--- docker-heroku/entrypoint.sh
+++ docker-heroku/entrypoint.sh
@@ -5,13 +5,13 @@ python manage.py makemigrations
 echo "<<< Migrating Database >>> "
 python manage.py migrate # migrate db
 
-sleep 2
+sleep 10
 echo "<<< Starting celery_config worker >>> "
 exec celery -A  celery_config.celery_app worker --loglevel=info  & # runs celery_config worker
-
+sleep 10
 echo "<<< Feeling the beat by spinning up celery_config-beat >>> "
 celery -A celery_config.celery_schedules beat --loglevel=info  & #runs celery_config beat
-sleep 5
+sleep 10
 echo "<<< Waiting for the celery_config-workers to flow with the beat >>> "
 
 echo "Starting server >>> "
